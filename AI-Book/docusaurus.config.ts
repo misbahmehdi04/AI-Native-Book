fffff0a9235724455693bd9315535cbcf08e0a73
misbahmehdi04 <misbahmehdi14@gmail.com>
2025-12-25 14:25:16 +0500

    Updated All Modules

--- AI-Book/docusaurus.config.ts
+++ AI-Book/docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://misbahmehdi04.github.io',
+  url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/AI-Native-Book/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
